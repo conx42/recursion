c442e65fc07433c9988b8745eec7071cb1b1a31f
conx42 <97751032+conx42@users.noreply.github.com>
2024-04-21 04:48:03 +0000

    Without->BackTracking_printing -> (1 to N) - Linearly

---
+++ Recursion_Striver_Level_1/readme.md
@@ -0,0 +1,1 @@
+[Playlist](https://www.youtube.com/playlist?list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9)
---
+++ Recursion_Striver_Level_1/vid_2/Main.java
@@ -0,0 +1,43 @@
+/**
+ * printing -> (1 to N) - Linearly
+ * Without -> BackTracking
+ */
+import java.util.*;
+public class Main{
+    public static void f(int i, int N){
+        if(i>N){
+            return;
+        }
+        System.out.println("i = "+i);
+        f(i+1,N);
+    }
+    public static void main(String []args){
+        Scanner sc = new  Scanner(System.in);
+        System.out.println("Program is Running...");
+        System.out.println("Enter n: ");
+        int n=sc.nextInt();
+        f(1,n);
+    }
+}
+
+/**
+* * //output
+*                         f(1, 5)                            |           
+*                        / -> check (1>5)? -> X (No)         |      Top                         
+*                       / -> print(1)                        |      Down       
+*                      f(2, 5)                               |      Linearly
+*                     / -> check (2>5)? -> X (No)            |                           
+*                    / -> print(2)                           |           
+*                   f(3, 5)                                  |   
+*                  / -> check (3>5)? -> X (No)               |                       
+*                 / -> print(3)                              |       
+*                f(4, 5)                                     |   
+*               / -> check (4>5)? -> X (No)                  |                   
+*              / -> print(4)                                 |       
+*             f(5, 5)                                        |
+*            / -> check (5>5)? -> X (No)                     |                   
+*           / -> print(5)                                    |   
+*          f(6, 5) --> X                                     |   
+*         |-> check (5>5)? -> X (Yes)                        |               
+*         return - (back-From-Here)                          |           
+*                                                               
